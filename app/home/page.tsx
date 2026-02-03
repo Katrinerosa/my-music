780041f6bf3c6a02ce11ba65c3df62b3c4a0c5ec
Katrinerosa <katrinerosa@gmail.com>
2026-02-03 12:53:57 +0100

    final

---
+++ app/home/page.tsx
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main className="mx-auto flex min-h-[400px] w-full max-w-[600px] flex-col px-6 pb-10 pt-6">
+      <h1 className="text-[24px] font-bold">Home</h1>
+      <p className="mt-2 text-[14px] text-black/70 dark:text-white/70">
+        Denne side er ikke i brug endnu.
+      </p>
+    </main>
+  );
+}
